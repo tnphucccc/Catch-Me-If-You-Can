bc3f1744d057024b016873bf8951f55fb961f629
Nguyễn Tuấn Khoa <khoanguyen030803@gmail.com>
2023-05-12 20:08:00 +0700

    Add Entity class and Player class

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -1,4 +1,31 @@
 package entities;
 
-public class Entity {
+import java.awt.*;
+import java.util.ArrayList;
+
+public abstract class Entity {
+    public int x, y, speed, state;
+    public String direction, name;
+    public Rectangle solidArea;
+
+    public boolean collisionOn = false;
+    public ArrayList<Integer> InteractionBox = new ArrayList<>();
+    public int solidAreaDefaultX, solidAreaDefaultY;
+
+
+    public abstract void update();
+    public void setEntityInteractionBox(Entity entity) {
+        this.InteractionBox.add(0, entity.y + entity.solidArea.y);//TopY
+        this.InteractionBox.add(1, entity.x + entity.solidArea.x + entity.solidArea.width);//RightX
+        this.InteractionBox.add(2, entity.y + entity.solidArea.y + entity.solidArea.height);//BottomY
+        this.InteractionBox.add(3, entity.x + entity.solidArea.x);//Left
+    }
+
+    //getter && setter for x y but in tile
+    public int getX() {
+        return ((x + 16) / 48) * 48;
+    }
+    public int getY() {
+        return ((y + 16) / 48) * 48;
+    }
 }
