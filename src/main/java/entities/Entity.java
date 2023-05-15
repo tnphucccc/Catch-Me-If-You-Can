5fdac5acd180763a32e24b1e654a3de640f3c3a7
Trần Nguyên Phúc <itcsiu21097@student.hcmiu.edu.vn>
2023-05-15 13:19:06 +0700

    update

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -24,6 +24,7 @@ public abstract class Entity {
         this.InteractionBox.add(2, entity.y + entity.solidArea.y + entity.solidArea.height);//BottomY
         this.InteractionBox.add(3, entity.x + entity.solidArea.x);//Left
     }
+
     public BufferedImage getEntityImage() {
         BufferedImage img = null;
         switch (direction) {
@@ -52,5 +53,13 @@ public abstract class Entity {
             }
         }
         return null;
+    }
+
+    //getter && setter for x y but in tile
+    public int getX() {
+        return ((x + 16) / 48) * 48;
+    }
+    public int getY() {
+        return ((y + 16) / 48) * 48;
     }
 }
